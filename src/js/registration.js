58160c48f3a3c889bfb79fa7c0b66c55be21dbae
konfetka4095-alt <konfetka4095@gmail.com>
2026-09-15 12:53:54 -0400

    Update registration.js

--- src/js/registration.js
+++ src/js/registration.js
@@ -40,11 +40,16 @@ var RegistrationModule = {
   /* =======================================================
      INIT
      ======================================================= */
-
   init: function () {
+
+    if (this._initialized) {
+      return;
+    }
+
+    this._initialized = true;
+
     this.bindEvents();
   },
-
 
   /* =======================================================
      EVENTS
